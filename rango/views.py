7424989e32b20a70b1e10dd2f52cdfd6901cf103
LewisDyer <2299195D@student.gla.ac.uk>
2019-01-15 10:37:36 +0000

    Completed up to and including Chapter 4.2: Serving Static Media Files

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there partner! Learn more about Rango <a href='/rango/about/'>here</a>")
+	# Dict is passed to template engine as context
+	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+	
+	# Returning a rendered response for the client
+	return render(request, 'rango/index.html', context=context_dict)
 	
 def about(request):
 	return HttpResponse("Rango says here is the about page. Head back to the main page <a href='/rango/'>here</a>")
