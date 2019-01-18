7757672e36180a9b96a596ea1eed40f5ecfa98fc
LewisDyer <2299195D@student.gla.ac.uk>
2019-01-18 15:35:24 +0000

    Completed Chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
+# adds ability to add pages on creation of a Category
+class PageInline(admin.StackedInline):
+	model = Page
+	extra = 3
+
+class CategoryAdmin(admin.ModelAdmin):
+	fieldsets = [(None,      {'fields':['name']}),
+	             ('Traffic info', {'fields': ['views', 'likes']}),
+				 ]
+				 
+	inlines = [PageInline]
+
+admin.site.register(Category, CategoryAdmin)
+	
+	
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('category', 'title', 'url')
+
+admin.site.register(Page, PageAdmin)
--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model): # inherits from models.Model, like all models
 	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
 	
 	class Meta:
 		verbose_name_plural = 'Categories' # don't just stick an 's' at the end!
