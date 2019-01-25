657c02c923bed9f9d1d885d5235e1980016837b4
LewisDyer <2299195D@student.gla.ac.uk>
2019-01-25 15:22:45 +0000

    Finished Chapter 7

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
 	# Dict is passed to template engine as context
@@ -41,3 +42,43 @@ def show_category(request, category_name_slug):
 		context_dict['category'] = None
 	
 	return render(request, 'rango/category.html', context_dict)
+
+	
+def add_category(request):
+	form = CategoryForm()
+	
+	# Received HTTP POST?
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		
+		if form.is_valid():
+			form.save(commit=True) # save new category to DB
+			
+			return index(request) # redirect to index page
+		else: # supplied form contains errors
+			print(form.errors)
+	
+	return render(request, 'rango/add_category.html', {'form':form})
+
+	
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+		
+	form = PageForm()
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+				return show_category(request, category_name_slug)
+		else:
+			print(form.errors)
+	
+	context_dict = {'form': form, 'category': category}
+	return render(request, 'rango/add_page.html', context_dict)
